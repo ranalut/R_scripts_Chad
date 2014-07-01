bcc7608381df77ab39a6ee98f4402cd6fbce96f6
ranalut <chadwilsey@gmail.com>
2014-07-01 09:13:17 -0700

    update make_dat_and_bat_files_for_spp_lists.r

--- make_dat_and_bat_files_for_spp_lists.R
+++ make_dat_and_bat_files_for_spp_lists.R
@@ -1,25 +1,26 @@
 
 # Settings
 source('gen.zig3.files.r')
-priority.flyway <- 'Any' # NA # 'Pacific' # 'Central' # 'Mississippi' # 'Atlantic' # 'Any'
-workspace <- paste("D:/Climate_Strongholds/flyways_analyses/",priority.flyway,"/",sep='')
+geography <- 'states'
+prioritize <- 'NC_coastal' # 'Any' # NA # 'Pacific' # 'Central' # 'Mississippi' # 'Atlantic' # 'Any'
+workspace <- paste("D:/Climate_Strongholds/",geography,"_analyses/",prioritize,"/",sep='')
 dir.create(paste(workspace,'run_files/',sep=''), recursive=TRUE)
 temp.dat.file <- read.csv("D:/Climate_Strongholds/prioritizations_single_species/dat_template.csv", stringsAsFactors=FALSE)
 
 # Priority species
-priority <- read.csv("D:/Climate_Strongholds/flyways_analyses/priority_species_22may14.csv", stringsAsFactors=FALSE)
-the.rows <- grep(priority.flyway, priority$Flyway, ignore.case=TRUE)
+priority <- read.csv("D:/Climate_Strongholds/",geography,"_analyses/priority_species_22may14.csv", stringsAsFactors=FALSE)
+the.rows <- grep('X', priority[,prioritize], ignore.case=TRUE)
 flyway.spp <- priority$BBL_ABBREV[the.rows]
-cat(priority.flyway,' priority species: ',flyway.spp,'\nNumber of Species: ',length(flyway.spp),'\n')
-# stop('cbw')
+cat(prioritize,' priority species: ',flyway.spp,'\nNumber of Species: ',length(flyway.spp),'\n')
+stop('cbw')
 
 # CBC
-cbc <- read.csv("D:/Climate_Strongholds/CBC_list_2014_03_07_all_species.csv",stringsAsFactors=FALSE)
+cbc <- read.csv("D:/Climate_Strongholds/CBC_list_2014_03_07_all_species.csv", stringsAsFactors=FALSE)
 
 gen.zig3.files(
 	theData=cbc, 
 	workspace=workspace, 
-	label=priority.flyway, 
+	label=prioritize, 
 	label.spp=flyway.spp,
 	season='CBC',
 	temp_dat=temp.dat.file,
@@ -32,7 +33,7 @@ bbs<-read.csv("D:/Climate_Strongholds/BBS_list_2014_03_07_all_species.csv",strin
 gen.zig3.files(
 	theData=cbc, 
 	workspace=workspace, 
-	label=priority.flyway, 
+	label=prioritize, 
 	label.spp=flyway.spp,
 	season='BBS',
 	temp_dat=temp.dat.file,
