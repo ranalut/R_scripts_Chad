75bd3cd27a7154dce5e06a0585b3b835bcadfa09
ranalut <chadwilsey@gmail.com>
2014-07-23 11:18:58 -0700

    update build.files.r

--- build.files.r
+++ build.files.r
@@ -1,22 +1,28 @@
+# Libraries
+library(raster)
+
 # Scripts
 source('gen.zig3.files.r')
 source('build.ensembles.r')
 
 # Settings
-geography <- 'misc' # 'states' # 'flyways'
-prioritize <- 'test2'
+geography <- 'flyways' # 'misc' # 'states' # 'flyways'
+prioritize <- 'Pacific'
 	# 'NC_pine_island_priority' # 'NC_pine_island_responsibility' # 'NC_coastal' 
 	# 'Any' # NA # 'Pacific' # 'Central' # 'Mississippi' # 'Atlantic' # 'Any'
 season <- c('CBC','BBS') # 'CBC' # 'BBS'
 true.false <- list(CBC=FALSE,BBS=TRUE)
 workspace <- paste("D:/Climate_Strongholds/analyses_",geography,"/",prioritize,"/",sep='')
-spp.file <- 'test_priority_species_1jul14.csv'
+spp.file <- 'priority_species_22may14.csv' # 'test_priority_species_1jul14.csv'
 
 # Priority species
 priority <- read.csv(paste("D:/Climate_Strongholds/analyses_",geography,"/",spp.file,sep=''), stringsAsFactors=FALSE)
 the.rows <- grep('X', priority[,prioritize], ignore.case=TRUE)
 select.spp <- priority$BBL_ABBREV[the.rows]
 cat(prioritize,' priority species: ',select.spp,'\nNumber of Species: ',length(select.spp),'\n')
+sink(paste(workspace, prioritize, '.txt',sep=''))
+	cat(prioritize,' priority species: ',select.spp,'\nNumber of Species: ',length(select.spp),'\n')
+sink()
 # stop('cbw')
 
 # Other inputs
@@ -41,13 +47,21 @@ for (k in season)
 		append.bat=true.false[[k]]
 		)
 }
-# stop('cbw')
+stop('cbw')
 
 # As yet, I cannot figure out how to use command line to run the batch file.
-readline(prompt="\n\nLoad and run the Zonation project\nThen press [enter] to continue")
-# command <- paste('zig3 -r D:/Climate_Strongholds/analyses_states/NC_coastal/NC_coastal.bat',,sep='')
-# command <- 'D: && cd D:/Climate_Strongholds/analyses_misc/test20/ && test20.bat'
+cat('zig3run d:\\climate_strongholds\\analyses_',geography,'\\',prioritize,'\\',prioritize,'.bat',sep='')
+readline(prompt='\n\nRun the Zonation batch project\nThen press [enter] to continue')
+
+# command <- "D: & cd D:/Climate_Strongholds/analyses_misc/test2/ & test2.bat"
+# command <- 'cd "C:\Program Files\zonation 3.1.11\bin\" & zig3run.exe "D:\Climate_Strongholds\analyses_misc\test2\test2.bat"'
+# command <- 'C: & cd "C:\\Program Files\\zonation 3.1.11\\bin" & zig3run.exe "D:\\Climate_Strongholds\\analyses_misc\\test2\\test2.bat"'
+# "D:\\Climate_Strongholds\\Zonation\\zig3run.exe \"D:\\Climate_Strongholds\\analyses_misc\\test2\\test2.bat\""
+
+# command <- 'zig3run.exe "D:\\Climate_Strongholds\\analyses_misc\\test2\\test2.bat"'
+
 # shell(command)
+# stop('cbw')
 	
 for (k in season)
 {
