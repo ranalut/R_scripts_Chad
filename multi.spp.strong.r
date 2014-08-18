18b380825101575e00636a4f8faf4a24125cf551
ranalut <chadwilsey@gmail.com>
2014-08-18 16:28:51 -0700

    update multi.spp.strong.r

--- multi.spp.strong.r
+++ multi.spp.strong.r
@@ -2,7 +2,8 @@
 # Run multi-species strongholds
 
 # Adjustable Settings
-spp.file <- 'az_multispecies_first_20140718.csv'
+spp.file <- 'mn_multispecies_no_grasslands.csv'
+	# 'az_multispecies_first_20140718.csv'
 	# "Climate Camp Species Lists - CLIMATE_CAMP_ALL.csv"
 	# 'priority_species_22may14.csv' 
 	# 'test_priority_species_1jul14.csv'
@@ -11,8 +12,8 @@ run.zonation <- TRUE
 priority <- read.csv(paste("D:/Climate_Strongholds/analyses_",geography,"/",spp.file,sep=''), stringsAsFactors=FALSE)
 
 requests <- colnames(priority)
-test <- colnames(priority) %in% c("AOU54_COMMON_NAME","AOU54_SPECIES","BBL_ABBREV","SUMMER","WINTER")
-requests <- requests[test==FALSE]
+	# test <- colnames(priority) %in% c("AOU54_COMMON_NAME","AOU54_SPECIES","BBL_ABBREV","SUMMER","WINTER")
+	# requests <- requests[test==FALSE]
 	# requests <- 'la_delta'
 	# 'fl_coastal' # 'tx_coastal' # 'tx_desert' # 'mn_grasslands' # 'la_delta' 
 	# 'test2'
