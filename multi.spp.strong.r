3aac408ce2f89bdf4ae88b4087fb12eef8f29d87
ranalut <chadwilsey@gmail.com>
2014-12-22 09:56:07 -0800

    update merge.performance

--- multi.spp.strong.r
+++ multi.spp.strong.r
@@ -2,14 +2,19 @@
 # Run multi-species strongholds
 
 # Adjustable Settings
-spp.file <- 'langham_priority_spp.csv'
+spp.file <- 'PacificFlyway_20141212_summer.csv'
+	# '314_sotb_hab_v1.csv' # '542_sotb_hab_v1.csv'# '542_hab_v1.csv' # '314_hab_v1.csv'
+	# 'mn_audubon_and_dnr_all_2.csv' # 'mn_audubon_and_dnr_tpr50_2.csv' # 'mn_audubon_and_dnr_all.csv' # 'mn_dnr_priority_spp.csv' # 'mn_multispecies_no_grasslands.csv'
+	# 'co_priority_spp2.csv' # 'co_priority_spp.csv'
+	# 'vt_priority_spp2.csv' # 'vt_priority_spp.csv'
+	# 'ar_priority_spp.csv'
+	# 'langham_priority_spp.csv'
 	# 'af_priority_spp.csv'
-	# 'mn_multispecies_no_grasslands.csv'
 	# 'az_multispecies_first_20140718.csv'
 	# "Climate Camp Species Lists - CLIMATE_CAMP_ALL.csv"
 	# 'priority_species_22may14.csv' 
 	# 'test_priority_species_1jul14.csv'
-geography <- 'misc' # 'misc' # 'states' # 'flyways'
+geography <- 'flyways' # 'misc' # 'states' # 'flyways'
 run.zonation <- TRUE
 priority <- read.csv(paste("D:/Climate_Strongholds/analyses_",geography,"/",spp.file,sep=''), stringsAsFactors=FALSE)
 previous <- '' # Initialize this counter
