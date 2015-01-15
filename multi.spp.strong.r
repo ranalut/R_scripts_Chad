d67ca04ff6e512aed085dea0479c76e2f19c3a01
ranalut <chadwilsey@gmail.com>
2015-01-15 10:11:55 -0800

    update multi.spp.strong.r

--- multi.spp.strong.r
+++ multi.spp.strong.r
@@ -2,14 +2,15 @@
 # Run multi-species strongholds
 
 # Adjustable Settings
-spp.file <- 'PacificFlyway_20141212_summer.csv'
+spp.file <- 
+	# 'PacificFlyway_20141212_summer.csv'
 	# '314_sotb_hab_v1.csv' # '542_sotb_hab_v1.csv'# '542_hab_v1.csv' # '314_hab_v1.csv'
-	# 'mn_audubon_and_dnr_all_2.csv' # 'mn_audubon_and_dnr_tpr50_2.csv' # 'mn_audubon_and_dnr_all.csv' # 'mn_dnr_priority_spp.csv' # 'mn_multispecies_no_grasslands.csv'
+	# 'mn_dnr_5jan15.csv' # 'mn_audubon_and_dnr_all_2.csv' # 'mn_audubon_and_dnr_tpr50_2.csv' # 'mn_audubon_and_dnr_all.csv' # 'mn_dnr_priority_spp.csv' # 'mn_multispecies_no_grasslands.csv'
 	# 'co_priority_spp2.csv' # 'co_priority_spp.csv'
 	# 'vt_priority_spp2.csv' # 'vt_priority_spp.csv'
 	# 'ar_priority_spp.csv'
 	# 'langham_priority_spp.csv'
-	# 'af_priority_spp.csv'
+	'af_forest_all_5jan15.csv' # 'af_priority_spp.csv'
 	# 'az_multispecies_first_20140718.csv'
 	# "Climate Camp Species Lists - CLIMATE_CAMP_ALL.csv"
 	# 'priority_species_22may14.csv' 
